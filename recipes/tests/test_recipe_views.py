f12363fe1c9a0a92222f9d710932feaf762670cf
Guilherme Ventura <guilhermeventurasousa@gmail.com>
2022-05-27 09:06:42 -0300

    testando a busca e enviando para a url correta

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -128,3 +128,8 @@ class RecipeViewsTest(RecipeTestBase):
         response = self.client.get(
             reverse('recipes:search'))  # Get na minha URL
         self.assertTemplateUsed(response, 'recipes/pages/search.html')
+
+    def test_recipe_search_raises_404_if_no_search_term(self):
+        url = reverse('recipes:search')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
