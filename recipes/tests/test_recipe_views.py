43046d971aceaffe8f61ec17d4d584c3f57207f4
Guilherme Ventura <guilhermeventurasousa@gmail.com>
2022-05-27 09:20:13 -0300

    corrigindo testes que quebraram

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -126,7 +126,7 @@ class RecipeViewsTest(RecipeTestBase):
 
     def test_recipe_search_loads_correct_template(self):
         response = self.client.get(
-            reverse('recipes:search'))  # Get na minha URL
+            reverse('recipes:search') + '?q=teste')  # Get na minha URL
         self.assertTemplateUsed(response, 'recipes/pages/search.html')
 
     def test_recipe_search_raises_404_if_no_search_term(self):
