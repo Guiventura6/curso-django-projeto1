4d2cd8af691285ea0118e0821d5f3104c273c3de
Guilherme Ventura <guilhermeventurasousa@gmail.com>
2022-06-04 15:55:48 -0300

    Criando uma função para criar mais de uma receita

--- tests/funtional_tests/recipes/test_recipe_home_page.py
+++ tests/funtional_tests/recipes/test_recipe_home_page.py
@@ -1,3 +1,5 @@
+from unittest.mock import patch
+
 import pytest
 from selenium.webdriver.common.by import By
 
@@ -6,6 +8,7 @@ from .base import RecipeBaseFuntionalTest
 
 @pytest.mark.functional_test
 class RecipeHomePageFunctionalTest(RecipeBaseFuntionalTest):
+    @patch('recipes.views.PER_PAGE', new=2)
     def test_recipe_home_page_without_recipes_error_not_found_message(self):
         self.browser.get(self.live_server_url)
         body = self.browser.find_element(By.TAG_NAME, 'body')
