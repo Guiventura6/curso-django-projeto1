b225fcf239d7e6c33de4f97d6411034740ed62d9
Guilherme Ventura <guilhermeventurasousa@gmail.com>
2022-06-08 18:31:44 -0300

    Criando a Search Listview

--- recipes/views.py
+++ recipes/views.py
@@ -55,6 +55,28 @@ class RecipeListViewCategory(RecipeListViewBase):
 class RecipeListViewSearch(RecipeListViewBase):
     template_name = 'recipes/pages/search.html'
 
+    def get_queryset(self, *args, **kwargs):
+        search_term = self.request.GET.get('q', '')
+        query_set = super().get_queryset(*args, **kwargs)
+        query_set = query_set.filter(
+            Q(
+                Q(title__icontains=search_term) |
+                Q(description__icontains=search_term),
+            ),
+        )
+        return query_set
+
+    def get_context_data(self, *args, **kwargs):
+        ctx = super().get_context_data(*args, **kwargs)
+        search_term = self.request.GET.get('q', '')
+        ctx.update({
+            'page_title': f'Search for "{search_term}" |',
+            'search_term': search_term,
+            'additional_url_query': f'&q={search_term}',
+        })
+
+        return ctx
+
 
 def home(request):
     recipes = Recipe.objects.filter(
