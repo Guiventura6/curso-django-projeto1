11b97c438dac3845082a9ba1ac38132b7dce98ed
Guilherme Ventura <guilhermeventurasousa@gmail.com>
2022-06-08 11:59:12 -0300

    Criando um metodo Post

--- authors/views/dashboard_recipe.py
+++ authors/views/dashboard_recipe.py
@@ -1,6 +1,6 @@
 from authors.forms.recipe_form import AuthorRecipeForm
 from django.contrib import messages
-from django.http import Http404
+from django.http.response import Http404
 from django.shortcuts import redirect, render
 from django.urls import reverse
 from django.views import View
@@ -8,25 +8,45 @@ from recipes.models import Recipe
 
 
 class DashboardRecipe(View):
+    def get_recipe(self, id):
+        recipe = None
+
+        if id:
+            recipe = Recipe.objects.filter(
+                is_published=False,
+                author=self.request.user,
+                pk=id,
+            ).first()
+
+            if not recipe:
+                raise Http404()
+
+        return recipe
+
+    def render_recipe(self, form):
+        return render(
+            self.request,
+            'authors/pages/dashboard_recipe.html',
+            context={
+                'form': form
+            }
+        )
+
     def get(self, request, id):
-        recipe = Recipe.objects.filter(
-            is_published=False,
-            author=request.user,
-            pk=id,
+        recipe = self.get_recipe(id)
+        form = AuthorRecipeForm(instance=recipe)
+        return self.render_recipe(form)
 
-        ).first()
-
-        if not recipe:
-            raise Http404()
-
+    def post(self, request, id):
+        recipe = self.get_recipe(id)
         form = AuthorRecipeForm(
             data=request.POST or None,
             files=request.FILES or None,
-            instance=recipe,
+            instance=recipe
         )
 
         if form.is_valid():
-            # Agora, o form é válido e eu posso tentar salvar.
+            # Agora, o form é válido e eu posso tentar salvar
             recipe = form.save(commit=False)
 
             recipe.author = request.user
@@ -40,10 +60,4 @@ class DashboardRecipe(View):
                 reverse('authors:dashboard_recipe_edit', args=(id,))
             )
 
-        return render(
-            request,
-            'authors/pages/dashboard_recipe.html',
-            context={
-                'form': form
-            }
-        )
+        return self.render_recipe(form)
