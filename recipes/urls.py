4ed6370617c957a9a007a82f11b608bdfc9992be
Guilherme Ventura <guilhermeventurasousa@gmail.com>
2022-06-15 18:57:11 -0300

    Adicionando JSONResponse para a API que usa ListView

--- recipes/urls.py
+++ recipes/urls.py
@@ -18,4 +18,7 @@ urlpatterns = [
 
     path('recipes/api/v1/',
          views.RecipeListViewHomeApi.as_view(), name="recipe_api_v1"),
+
+    path('recipes/api/v1/<int:pk>/',
+         views.RecipeDetailAPI.as_view(), name="recipe_api_v1_detail"),
 ]
