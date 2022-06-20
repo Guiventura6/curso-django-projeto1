51ab0af01a14220fa58c1196a37b15e11c5671ad
Guilherme Ventura <guilhermeventurasousa@gmail.com>
2022-06-20 11:54:11 -0300

    Transformando o arquivo settings em uma pasta organizada

---
+++ project/settings/installed_apps.py
@@ -0,0 +1,14 @@
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    # também posso criar apps e adiciona-los aqui!
+    'recipes',
+    'authors',
+    'tag',
+
+]
