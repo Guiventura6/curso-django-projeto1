bdfc23b9d86fc383c717c12513697886f9f4e969
Guilherme Ventura <guilhermeventurasousa@gmail.com>
2022-07-14 12:07:37 -0300

    Autenticação de usuários com JWT no Django Rest Framework

--- project/settings/installed_apps.py
+++ project/settings/installed_apps.py
@@ -8,6 +8,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Django rest framework
+    'rest_framework_simplejwt',
     'rest_framework',
     # Estes são nossos apps
     'recipes',
